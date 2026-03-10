2546d9c34ff42849d9c8d97da7c67f921d12b8dc
Rizwanul Islam <afraim.afraim99@gmail.com>
2026-03-10 07:57:03 +0600

    Update SEO redirects to 2026

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,55 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/blog/the-top-10-white-label-seo-companies-for-2023',
+        destination: '/blog/the-top-10-white-label-seo-companies-for-2026',
+        permanent: true,
+      },
+      {
+        source: '/blog/on-page-seo-techniques-in-2023',
+        destination: '/blog/on-page-seo-techniques-in-2026',
+        permanent: true,
+      },
+      {
+        source: '/blog/how-to-do-seo-for-b2c-companies-in-2024',
+        destination: '/blog/how-to-do-seo-for-b2c-companies-in-2026',
+        permanent: true,
+      },
+      {
+        source: '/blog/seo-myths-that-you-should-never-follow-in-2024',
+        destination: '/blog/seo-myths-that-you-should-never-follow-in-2026',
+        permanent: true,
+      },
+      {
+        source: '/blog/best-reputation-management-companies-of-2024',
+        destination: '/blog/best-reputation-management-companies-of-2026',
+        permanent: true,
+      },
+      {
+        source: '/blog/how-much-does-google-ads-cost-in-2024',
+        destination: '/blog/how-much-does-google-ads-cost-in-2026',
+        permanent: true,
+      },
+      {
+        source: '/blog/8-important-b2b-marketing-trends-for-2024',
+        destination: '/blog/8-important-b2b-marketing-trends-for-2026',
+        permanent: true,
+      },
+      {
+        source: '/blog/17-top-programmatic-advertising-platforms-to-use-in-2023',
+        destination: '/blog/17-top-programmatic-advertising-platforms-to-use-in-2026',
+        permanent: true,
+      },
+      {
+        source: '/blog/8-important-b2b-marketing-trends-for-2024-2',
+        destination: '/blog/8-important-b2b-marketing-trends-for-2026-2',
+        permanent: true,
+      }
+    ]
+  }
 };
 
 export default nextConfig;
